f0bc2e7ae29aa39c6f4579930fcb0257c5047d05
Seth Frey <moctodliamg@gmail.com>
2024-06-23 15:26:53 -0400

    Update __init__.py
    tweak source site

--- image_play/__init__.py
+++ image_play/__init__.py
@@ -32,7 +32,8 @@ def placekittendotcomURL(w, h, color=True):
   Construct URL for placekitten.com from parameters.
   """
   greyscale = "" if color else "g/"
-  URL="http://placekitten.com/{}{}/{}".format( greyscale, w, h ) 
+  #URL="http://placekitten.com/{}{}/{}".format( greyscale, w, h ) 
+  #URL="https://loremflickr.com/{}{}/{}/cat".format( greyscale, w, h )
   return( URL )
   
 # https://www.pyimagesearch.com/2015/03/02/convert-url-to-image-with-python-and-opencv/
